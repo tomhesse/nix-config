29ff99f1a4d7953a5451969e4995953ccdfc4ed2
Tom Hesse <contact@tomhesse.xyz>
2025-11-04 21:41:48 +0100

    feat(home/core/nixvim): add bufferline config

---
+++ home/tom/core/nixvim/plugins/bufferline.nix
@@ -0,0 +1,24 @@
+{
+  plugins.bufferline = {
+    enable = true;
+    settings = {
+      options = {
+        mode = "tabs";
+        diagnostics = "nvim_lsp";
+        diagnostics_indicator = ''
+          function(count, level, diagnostics_dict, context)
+            local s = ""
+            for e, n in pairs(diagnostics_dict) do
+              local sym = e == "error" and " "
+                or (e == "warning" and " " or "" )
+              if(sym ~= "") then
+                s = s .. n .. sym
+              end
+            end
+            return s
+          end
+        '';
+      };
+    };
+  };
+}
