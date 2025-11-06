6deb77a856d405075b20ba35f314699daca9180c
Tom Hesse <contact@tomhesse.xyz>
2025-11-06 20:18:24 +0100

    refactor(home/core/nixvim): change diagnostics symbols for lualine and bufferline

--- home/tom/core/nixvim/plugins/bufferline.nix
+++ home/tom/core/nixvim/plugins/bufferline.nix
@@ -7,15 +7,8 @@
         diagnostics = "nvim_lsp";
         diagnostics_indicator = ''
           function(count, level, diagnostics_dict, context)
-            local s = ""
-            for e, n in pairs(diagnostics_dict) do
-              local sym = e == "error" and " "
-                or (e == "warning" and " " or "" )
-              if(sym ~= "") then
-                s = s .. n .. sym
-              end
-            end
-            return s
+            local icon = level:match("error") and "E " or "W "
+            return " " .. icon .. count
           end
         '';
       };
--- home/tom/core/nixvim/plugins/lualine.nix
+++ home/tom/core/nixvim/plugins/lualine.nix
@@ -1,3 +1,22 @@
 {
-  plugins.lualine.enable = true;
+  plugins.lualine = {
+    enable = true;
+    settings = {
+      sections = {
+        lualine_b = [
+          "branch"
+          "diff"
+          {
+            __unkeyed = "diagnostics";
+            symbols = {
+              error = "E";
+              warn = "W";
+              info = "I";
+              hint = "H";
+            };
+          }
+        ];
+      };
+    };
+  };
 }
