fb615532844bd7ccbd61df662cf171d2494ce38b
Tom Hesse <contact@tomhesse.xyz>
2025-11-06 11:41:54 +0100

    feat(home/core/nixvim): add nixd lsp server

--- home/tom/core/nixvim/plugins/lsp.nix
+++ home/tom/core/nixvim/plugins/lsp.nix
@@ -1,8 +1,25 @@
+{ hmConfig, ... }:
 {
   plugins.lsp = {
     enable = true;
+    inlayHints = true;
     servers = {
-      nixd.enable = true;
+      nixd = {
+        enable = true;
+        settings =
+          let
+            flake = "(builtins.getFlake (builtins.toString ./.))";
+            inherit (hmConfig) hostName;
+          in
+          {
+            options = {
+              nixos.expr = "${flake}.nixosConfigurations.${hostName}.options";
+              home-manager.expr = "${flake}.nixosConfigurations.${hostName}.options.home-manager.users.type.getSubOptions []";
+              nixvim.expr = "(${flake}.inputs.nixvim.lib.evalNixvim { system = builtins.currentSystem; }).options";
+              flake-parts.expr = "${flake}.debug.options // ${flake}.currentSystem.options";
+            };
+          };
+      };
     };
   };
 }
