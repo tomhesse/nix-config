de2249c91b42d7f28bfc489863d9e4224ae04924
Tom Hesse <contact@tomhesse.xyz>
2025-11-09 11:08:04 +0100

    feat(home/desktop/hyprland): make pavucontrol window float

--- home/tom/desktop/hyprland/windowrules.nix
+++ home/tom/desktop/hyprland/windowrules.nix
@@ -1,6 +1,7 @@
 {
   wayland.windowManager.hyprland.settings = {
     windowrule = [
+      "float, class:org.pulseaudio.pavucontrol"
       "opacity 0.95, focus:0"
       "opacity 1.00, focus:1"
     ];
