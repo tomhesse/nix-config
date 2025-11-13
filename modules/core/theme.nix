9a96833ad4e9c05a7b67a81d6e79cabe2b7a1811
Tom Hesse <contact@tomhesse.xyz>
2025-11-13 17:43:45 +0100

    feat(modules/core/theme): add fontSize option for catppuccin theme

--- modules/core/theme.nix
+++ modules/core/theme.nix
@@ -5,14 +5,26 @@
   ...
 }:
 let
-  inherit (lib) mkEnableOption mkIf;
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    mkOption
+    types
+    ;
 
   cfg = config.hostSpec.theme;
 in
 {
   imports = [ inputs.catppuccin.nixosModules.catppuccin ];
 
-  options.hostSpec.theme.catppuccin.enable = mkEnableOption "Enable catppuccin theme.";
+  options.hostSpec.theme.catppuccin = {
+    enable = mkEnableOption "Enable catppuccin theme.";
+    fontSize = mkOption {
+      type = types.str;
+      default = "12";
+      description = "Font size used by different catppuccin ports.";
+    };
+  };
 
   config = mkIf cfg.catppuccin.enable {
     catppuccin.enable = true;
