8d7e281d11541f76f4c1f6da41c4d1effe707fd3
Tom Hesse <contact@tomhesse.xyz>
2025-11-13 17:44:37 +0100

    feat(modules/desktop/display-manager): add display-manager config

---
+++ modules/desktop/display-manager.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+
+  cfg = config.hostSpec;
+in
+{
+  config = mkIf cfg.desktop.enable {
+    catppuccin.sddm = mkIf cfg.theme.catppuccin.enable {
+      inherit (cfg.theme.catppuccin) fontSize;
+      font = "Fira Code";
+    };
+
+    fonts = mkIf cfg.theme.catppuccin.enable {
+      packages = [ pkgs.fira-code ];
+    };
+
+    services.displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+      package = pkgs.kdePackages.sddm;
+    };
+  };
+}
