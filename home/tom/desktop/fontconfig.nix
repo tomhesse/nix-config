7a1440cc790803716e482237253a65a8a31b7721
Tom Hesse <contact@tomhesse.xyz>
2025-11-14 22:34:23 +0100

    feat(home/tom/desktop/fontconfig): add fontconfig setup

---
+++ home/tom/desktop/fontconfig.nix
@@ -0,0 +1,16 @@
+{
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      monospace = [
+        "FiraCode Nerd Font Mono"
+      ];
+      sansSerif = [
+        "Fira Sans"
+      ];
+      serif = [
+        "Fira Sans"
+      ];
+    };
+  };
+}
