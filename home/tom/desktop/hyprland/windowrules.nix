ee08346d3f7252b418fa3286ba01cadf50ba7118
Tom Hesse <contact@tomhesse.xyz>
2025-11-15 20:34:02 +0100

    feat(home/tom/desktop/hyprland): add windowrule for pavucontrol

--- home/tom/desktop/hyprland/windowrules.nix
+++ home/tom/desktop/hyprland/windowrules.nix
@@ -8,6 +8,7 @@ in
   wayland.windowManager.hyprland = mkIf enabled {
     settings = {
       windowrule = [
+        "float, class:org.pulseaudio.pavucontrol"
         "opacity 0.95, focus:0"
         "opacity 1.00, focus:1"
       ];
