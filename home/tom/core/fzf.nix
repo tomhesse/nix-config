dcccdf4f312360560ff84c15a28ed50a3c82cdcf
Tom Hesse <contact@tomhesse.xyz>
2025-11-15 10:43:44 +0100

    fix(home/tom/core/fzf): fix fzf alt+c options

--- home/tom/core/fzf.nix
+++ home/tom/core/fzf.nix
@@ -14,7 +14,7 @@ in
   programs.fzf = {
     enable = true;
     changeDirWidgetCommand = "${fd} --type=d --hidden --strip-cwd-prefix --exclude .git";
-    changeDirWidgetOptions = [ "--preview '${eza} --tree --color=always {} ${head} -200'" ];
+    changeDirWidgetOptions = [ "--preview '${eza} --tree --color=always {} | ${head} -200'" ];
     defaultCommand = "fd --type=f --hidden --strip-cwd-prefix --exclude .git";
     fileWidgetCommand = config.programs.fzf.defaultCommand;
     fileWidgetOptions = [ "--preview '${bat} -n --color=always --line-range :500 {}'" ];
