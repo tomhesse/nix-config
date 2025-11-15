f7dacedb1d4e1e1982835bd74d820936d32e0048
Tom Hesse <contact@tomhesse.xyz>
2025-11-15 18:40:37 +0100

    refactor(home/tom/core/fzf): use nix path for fd

--- home/tom/core/fzf.nix
+++ home/tom/core/fzf.nix
@@ -15,7 +15,7 @@ in
     enable = true;
     changeDirWidgetCommand = "${fd} --type=d --hidden --strip-cwd-prefix --exclude .git";
     changeDirWidgetOptions = [ "--preview '${eza} --tree --color=always {} | ${head} -200'" ];
-    defaultCommand = "fd --type=f --hidden --strip-cwd-prefix --exclude .git";
+    defaultCommand = "${fd} --type=f --hidden --strip-cwd-prefix --exclude .git";
     fileWidgetCommand = config.programs.fzf.defaultCommand;
     fileWidgetOptions = [ "--preview '${bat} -n --color=always --line-range :500 {}'" ];
   };
