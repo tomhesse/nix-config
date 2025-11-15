adc4e4ab630d9cd9d254ae7cbc4d3292835d9e79
Tom Hesse <contact@tomhesse.xyz>
2025-11-15 17:16:10 +0100

    feat(home/tom/desktop/xdg): add xdg directories

---
+++ home/tom/desktop/xdg.nix
@@ -0,0 +1,48 @@
+{
+  config,
+  lib,
+  osConfig,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+
+  impermanenceEnabled = osConfig.hostSpec.impermanence.enable;
+
+  relativeUserDir = dir: lib.removePrefix "${config.home.homeDirectory}/" dir;
+in
+{
+  xdg = {
+    autostart = {
+      enable = true;
+      readOnly = true;
+    };
+    userDirs = {
+      enable = true;
+      desktop = null;
+      publicShare = null;
+      templates = null;
+      documents = "${config.home.homeDirectory}/documents";
+      download = "${config.home.homeDirectory}/download";
+      music = "${config.home.homeDirectory}/music";
+      pictures = "${config.home.homeDirectory}/pictures";
+      videos = "${config.home.homeDirectory}/videos";
+      extraConfig = {
+        XDG_PROJECTS_DIR = "${config.home.homeDirectory}/projects";
+      };
+    };
+  };
+
+  home.persistence = mkIf impermanenceEnabled {
+    "/persist${config.home.homeDirectory}" = {
+      directories = [
+        (relativeUserDir config.xdg.userDirs.documents)
+        (relativeUserDir config.xdg.userDirs.download)
+        (relativeUserDir config.xdg.userDirs.music)
+        (relativeUserDir config.xdg.userDirs.pictures)
+        (relativeUserDir config.xdg.userDirs.videos)
+        (relativeUserDir config.xdg.userDirs.extraConfig.XDG_PROJECTS_DIR)
+      ];
+    };
+  };
+}
