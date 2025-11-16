eea35ba6b40c5cea5e8910dd34b94d0ed5d9d773
Tom Hesse <contact@tomhesse.xyz>
2025-11-16 17:13:55 +0100

    feat(modules/core/theme): add catppuccin.flavor and catppuccin.accent options

--- modules/core/theme.nix
+++ modules/core/theme.nix
@@ -19,6 +19,36 @@ in
 
   options.hostSpec.theme.catppuccin = {
     enable = mkEnableOption "Enable catppuccin theme.";
+    flavor = mkOption {
+      type = types.enum [
+        "latte"
+        "frappe"
+        "macchiato"
+        "mocha"
+      ];
+      default = "mocha";
+      description = "Flavor used by different catppuccin ports.";
+    };
+    accent = mkOption {
+      type = types.enum [
+        "blue"
+        "flamingo"
+        "green"
+        "lavender"
+        "maroon"
+        "mauve"
+        "peach"
+        "pink"
+        "red"
+        "rosewater"
+        "sapphire"
+        "sky"
+        "teal"
+        "yellow"
+      ];
+      default = "mauve";
+      description = "Accent used by different catppuccin ports.";
+    };
     fontSize = mkOption {
       type = types.str;
       default = "12";
