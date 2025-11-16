c36ec6fe371164a558221d4fccf40a49b9ff45e9
Tom Hesse <contact@tomhesse.xyz>
2025-11-16 17:19:28 +0100

    feat(home/tom/core/theme): add catppuccin.flavor and catppuccin.accent options

--- home/tom/core/theme.nix
+++ home/tom/core/theme.nix
@@ -13,10 +13,42 @@ in
 {
   imports = [ inputs.catppuccin.homeModules.catppuccin ];
 
-  options.homeSpec.theme.catppuccin.enable = mkOption {
-    type = types.bool;
-    default = osConfig.hostSpec.theme.catppuccin.enable;
-    description = "Enable catppucin theme for home-manager modules.";
+  options.homeSpec.theme.catppuccin = {
+    enable = mkOption {
+      type = types.bool;
+      default = osConfig.hostSpec.theme.catppuccin.enable;
+      description = "Enable catppucin theme for home-manager modules.";
+    };
+    flavor = mkOption {
+      type = types.enum [
+        "latte"
+        "frappe"
+        "macchiato"
+        "mocha"
+      ];
+      default = osConfig.hostSpec.theme.catppuccin.flavor;
+      description = "Flavor used by different catppuccin ports.";
+    };
+    accent = mkOption {
+      type = types.enum [
+        "blue"
+        "flamingo"
+        "green"
+        "lavender"
+        "maroon"
+        "mauve"
+        "peach"
+        "pink"
+        "red"
+        "rosewater"
+        "sapphire"
+        "sky"
+        "teal"
+        "yellow"
+      ];
+      default = osConfig.hostSpec.theme.catppuccin.accent;
+      description = "Accent used by different catppuccin ports.";
+    };
   };
 
   config.catppuccin = mkIf cfg.catppuccin.enable {
