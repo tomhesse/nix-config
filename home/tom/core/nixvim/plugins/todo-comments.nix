4bc40ad8f09d147e3c1e654c0ab9c494e6928cfb
Tom Hesse <contact@tomhesse.xyz>
2025-11-17 14:17:58 +0100

    feat(home/tom/core/nixvim): add todo-comments plugin

---
+++ home/tom/core/nixvim/plugins/todo-comments.nix
@@ -0,0 +1,10 @@
+{
+  plugins.todo-comments = {
+    enable = true;
+    keymaps.todoTelescope.key = "<leader>ft";
+    keymaps.todoTrouble = {
+      action = "Trouble todo toggle";
+      key = "<leader>xt";
+    };
+  };
+}
