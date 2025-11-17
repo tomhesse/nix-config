106325e82114350324ed01431aa655cb0ebd4204
Tom Hesse <contact@tomhesse.xyz>
2025-11-17 20:32:37 +0100

    feat(home/tom/core/nixvim): add autopairs integration for nvim-cmp

--- home/tom/core/nixvim/plugins/completion.nix
+++ home/tom/core/nixvim/plugins/completion.nix
@@ -5,6 +5,14 @@
       luaConfig.pre = ''
         local luasnip = require('luasnip')
       '';
+      luaConfig.post = ''
+        local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+        local cmp = require('cmp')
+        cmp.event:on(
+          'confirm_done',
+          cmp_autopairs.on_confirm_done()
+        )
+      '';
       cmdline = {
         "/" = {
           mapping = {
