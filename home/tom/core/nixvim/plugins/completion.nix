0dac551ac1e707074778eab81f5abf4781e4777c
Tom Hesse <contact@tomhesse.xyz>
2025-11-17 19:15:22 +0100

    feat(home/tom/core/nixvim): add completions setup

---
+++ home/tom/core/nixvim/plugins/completion.nix
@@ -0,0 +1,89 @@
+{
+  plugins = {
+    cmp = {
+      enable = true;
+      luaConfig.pre = ''
+        local luasnip = require('luasnip')
+      '';
+      cmdline = {
+        "/" = {
+          mapping = {
+            __raw = "cmp.mapping.preset.cmdline()";
+          };
+          sources = [ { name = "buffer"; } ];
+        };
+        ":" = {
+          mapping = {
+            __raw = "cmp.mapping.preset.cmdline()";
+          };
+          sources = [
+            { name = "path"; }
+            {
+              name = "cmdline";
+              option = {
+                ignore_cmds = [
+                  "Man"
+                  "!"
+                ];
+              };
+            }
+          ];
+        };
+      };
+      settings = {
+        mapping = {
+          "<C-k>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-j>" = "cmp.mapping.scroll_docs(4)";
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-e>" = "cmp.mapping.abort()";
+          "<CR>" = ''
+            cmp.mapping(function(fallback)
+              if cmp.visible() then
+                if luasnip.expandable() then
+                  luasnip.expand()
+                else
+                  cmp.confirm({
+                    select = true,
+                  })
+                end
+              else
+                fallback()
+              end
+            end)
+          '';
+          "<Tab>" = ''
+            cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif luasnip.locally_jumpable(1) then
+                luasnip.jump(1)
+              else
+                fallback()
+              end
+            end, { "i", "s" })
+          '';
+          "<S-Tab>" = ''
+            cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.locally_jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end, { "i", "s" })
+          '';
+        };
+        sources = [
+          { name = "nvim_lsp"; }
+          { name = "path"; }
+          { name = "buffer"; }
+          { name = "luasnip"; }
+        ];
+      };
+    };
+    luasnip.enable = true;
+    friendly-snippets.enable = true;
+    lspkind.enable = true;
+  };
+}
