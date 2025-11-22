8a01169cc233a02a90b5e0a267c50b1c7dc5d739
Tom Hesse <contact@tomhesse.xyz>
2025-11-22 15:12:01 +0100

    feat(home/tom/gaming/steam): add autostart desktop file

--- home/tom/gaming/steam.nix
+++ home/tom/gaming/steam.nix
@@ -2,6 +2,7 @@
   config,
   lib,
   osConfig,
+  pkgs,
   ...
 }:
 let
@@ -11,9 +12,23 @@ let
   impermanenceEnabled = osConfig.hostSpec.impermanence.enable;
 
   relativeDataHome = lib.removePrefix "${config.home.homeDirectory}/" config.xdg.dataHome;
+
+  steamDesktop = pkgs.makeDesktopItem {
+    name = "steam";
+    desktopName = "Steam";
+    exec = "steam -silent %U";
+    icon = "steam";
+    terminal = false;
+    type = "Application";
+    prefersNonDefaultGPU = true;
+  };
 in
 {
   config = mkIf enabled {
+    xdg.autostart.entries = [
+      "${steamDesktop}/share/applications/steam.desktop"
+    ];
+
     home.persistence = mkIf impermanenceEnabled {
       "/persist${config.home.homeDirectory}" = {
         directories = [
