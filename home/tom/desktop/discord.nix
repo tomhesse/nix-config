d5c22d692d1e333c11c54900eba9277f70b5d036
Tom Hesse <contact@tomhesse.xyz>
2025-11-22 19:26:36 +0100

    feat(home/tom/desktop/discord): make autostart optional

--- home/tom/desktop/discord.nix
+++ home/tom/desktop/discord.nix
@@ -6,70 +6,76 @@
   ...
 }:
 let
-  inherit (lib) mkIf;
+  inherit (lib) mkEnableOption mkIf;
 
   jsonFormat = pkgs.formats.json { };
 
   impermanenceEnabled = osConfig.hostSpec.impermanence.enable;
 
+  cfg = config.homeSpec.desktop.discord;
+
   relativeConfigHome = lib.removePrefix "${config.home.homeDirectory}/" config.xdg.configHome;
 in
 {
-  programs.vesktop = {
-    enable = true;
-    settings = {
-      discordBranch = "stable";
-      tray = true;
-      minimizeToTray = true;
-      autoStartMinimized = true;
-      hardwareAcceleration = true;
-      arRPC = true;
-      appBadge = true;
-      clickTrayToShowHide = true;
-      enableSplashScreen = true;
-      splashTheming = true;
-      splashColor = "rgb(203, 166, 247)";
-      splashBackground = "rgb(24, 24, 37)";
-      spellCheckLanguages = [
-        "en"
-        "en-US"
-        "de-DE"
-      ];
-    };
-    vencord = {
+  options.homeSpec.desktop.discord.autostart = mkEnableOption "Autostart discord on loin.";
+
+  config = {
+    programs.vesktop = {
+      enable = true;
       settings = {
-        notifications = {
-          timeout = 5000;
-          position = "bottom-right";
-          useNative = "not-focused";
-          logLimit = 50;
-        };
-        plugins = {
-          BetterFolders = {
-            enabled = true;
-            closeAllFolders = true;
-            closeAllHomeButton = true;
-            closeOthers = true;
+        discordBranch = "stable";
+        tray = true;
+        minimizeToTray = true;
+        autoStartMinimized = true;
+        hardwareAcceleration = true;
+        arRPC = true;
+        appBadge = true;
+        clickTrayToShowHide = true;
+        enableSplashScreen = true;
+        splashTheming = true;
+        splashColor = "rgb(203, 166, 247)";
+        splashBackground = "rgb(24, 24, 37)";
+        spellCheckLanguages = [
+          "en"
+          "en-US"
+          "de-DE"
+        ];
+      };
+      vencord = {
+        settings = {
+          notifications = {
+            timeout = 5000;
+            position = "bottom-right";
+            useNative = "not-focused";
+            logLimit = 50;
+          };
+          plugins = {
+            BetterFolders = {
+              enabled = true;
+              closeAllFolders = true;
+              closeAllHomeButton = true;
+              closeOthers = true;
+            };
           };
         };
+        useSystem = true;
       };
-      useSystem = true;
     };
-  };
 
-  home.file."${relativeConfigHome}/vesktop/state.json".source = jsonFormat.generate "vesktop-state" {
-    firstLaunch = true;
-  };
+    home.file."${relativeConfigHome}/vesktop/state.json".source = jsonFormat.generate "vesktop-state" {
+      firstLaunch = true;
+    };
 
-  xdg.autostart.entries = [
-    "${pkgs.vesktop}/share/applications/vesktop.desktop"
-  ];
+    xdg.autostart.entries = mkIf cfg.autostart [
+      "${pkgs.vesktop}/share/applications/vesktop.desktop"
+    ];
 
-  home.persistence = mkIf impermanenceEnabled {
-    "/persist${config.home.homeDirectory}" = {
-      directories = [
-        "${relativeConfigHome}/vesktop/sessionData"
-      ];
+    home.persistence = mkIf impermanenceEnabled {
+      "/persist${config.home.homeDirectory}" = {
+        directories = [
+          "${relativeConfigHome}/vesktop/sessionData"
+        ];
+      };
     };
   };
 }
