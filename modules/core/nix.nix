7a7162e4b1063d5630068db41b0ab322245e4a7d
Tom Hesse <contact@tomhesse.xyz>
2025-11-23 21:23:27 +0100

    feat(modules/core/nix): update nix settings

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -1,10 +1,14 @@
 {
   nix = {
     settings = {
+      connect-timeout = 5;
+      min-free = "${toString (100 * 1024 * 1024)}";
+      max-free = "${toString (1024 * 1024 * 1024)}";
       experimental-features = [
         "flakes"
         "nix-command"
       ];
+      auto-optimise-store = true;
     };
   };
 }
