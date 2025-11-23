34008f012863288b2aa0bb42cde3142fe48fd35e
Tom Hesse <contact@tomhesse.xyz>
2025-11-23 12:24:26 +0100

    feat(home/tom/desktop/mpd): add mpd config

---
+++ home/tom/desktop/mpd.nix
@@ -0,0 +1,33 @@
+{
+  config,
+  lib,
+  osConfig,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+
+  impermanenceEnabled = osConfig.hostSpec.impermanence.enable;
+
+  relativeDataHome = lib.removePrefix "${config.home.homeDirectory}/" config.xdg.dataHome;
+in
+{
+  services.mpd = {
+    enable = true;
+    extraConfig = ''
+      audio_output {
+        type "pipewire"
+        name "PipeWire Sound Server"
+      }
+      restore_paused "yes"
+    '';
+  };
+
+  home.persistence = mkIf impermanenceEnabled {
+    "/persist${config.home.homeDirectory}" = {
+      directories = [
+        "${relativeDataHome}/mpd"
+      ];
+    };
+  };
+}
