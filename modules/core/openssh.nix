81c6aa6e8ba9166a194a925bcb168c04c60b4ae8
Tom Hesse <contact@tomhesse.xyz>
2025-11-29 15:24:50 +0100

    feat(modules/core/openssh): add codeberg.org host keys

--- modules/core/openssh.nix
+++ modules/core/openssh.nix
@@ -21,6 +21,12 @@ let
 
   keyPathFor = host: "${inputs.self}/hosts/${host}/ssh_host_ed25519_key.pub";
 
+  codebergHostKeys = pkgs.writeText "codeberg-host-keys" ''
+    codeberg.org ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8hZi7K1/2E2uBX8gwPRJAHvRAob+3Sn+y2hxiEhN0buv1igjYFTgFO2qQD8vLfU/HT/P/rqvEeTvaDfY1y/vcvQ8+YuUYyTwE2UaVU5aJv89y6PEZBYycaJCPdGIfZlLMmjilh/Sk8IWSEK6dQr+g686lu5cSWrFW60ixWpHpEVB26eRWin3lKYWSQGMwwKv4LwmW3ouqqs4Z4vsqRFqXJ/eCi3yhpT+nOjljXvZKiYTpYajqUC48IHAxTWugrKe1vXWOPxVXXMQEPsaIRc2hpK+v1LmfB7GnEGvF1UAKnEZbUuiD9PBEeD5a1MZQIzcoPWCrTxipEpuXQ5Tni4mN
+    codeberg.org ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBL2pDxWr18SoiDJCGZ5LmxPygTlPu+cCKSkpqkvCyQzl5xmIMeKNdfdBpfbCGDPoZQghePzFZkKJNR/v9Win3Sc=
+    codeberg.org ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIVIC02vnjFyL+I4RHfvIGNtOgJMe769VTF1VR4EB3ZB
+  '';
+
   githubHostKeys = pkgs.fetchurl {
     url = "https://api.github.com/meta";
     name = "github-host-keys";
@@ -55,6 +61,9 @@ in
         (optionals (domain != null) [ "${host}.${domain}" ]) ++ (optional (host == hostName) "localhost")
       );
     });
-    knownHostsFiles = [ githubHostKeys ];
+    knownHostsFiles = [
+      codebergHostKeys
+      githubHostKeys
+    ];
   };
 }
