acc8c04e3c60469ee1bc3a0afd74292b1c01e6c3
Tom Hesse <contact@tomhesse.xyz>
2025-12-03 18:58:54 +0100

    feat(modules/desktop): add obsidian to allowedUnfree

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -8,4 +8,8 @@ in
   imports = importAll ./.;
 
   options.hostSpec.desktop.enable = mkEnableOption "Enable desktop environment on this system.";
+
+  config.hostSpec.allowedUnfree = [
+    "obsidian"
+  ];
 }
