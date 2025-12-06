ff599651b4dd5bf351348303bb788b1932d99c49
Tom Hesse <contact@tomhesse.xyz>
2025-12-06 16:18:41 +0100

    feat(modules/core/boot): refactor secureboot setup and add cryptenroll script

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -1,10 +1,60 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   inherit (lib) mkEnableOption mkIf;
 
   cfg = config.hostSpec.boot;
 
+  diskoLayout = config.hostSpec.disko.layout;
+
   impermanenceEnabled = config.hostSpec.impermanence.enable;
+
+  diskDevices = lib.attrsToList (config.disko.devices.disk or { });
+
+  luksPartitions = lib.concatMap (
+    disk:
+    let
+      diskName = disk.name;
+      diskConfig = disk.value;
+      partitions = diskConfig.content.partitions or { };
+    in
+    lib.mapAttrsToList (
+      partName: partConfig:
+      let
+        partitionType = partConfig.content.type or "";
+      in
+      {
+        inherit diskName partName partitionType;
+      }
+    ) partitions
+  ) diskDevices;
+
+  luksPartition = lib.findFirst (partition: partition.partitionType == "luks") null luksPartitions;
+
+  luksDevice =
+    if luksPartition == null then
+      null
+    else
+      "/dev/disk/by-partlabel/disk-${luksPartition.diskName}-${luksPartition.partName}";
+
+  cryptenroll = pkgs.writeShellApplication {
+    name = "cryptenroll";
+    runtimeInputs = [ pkgs.systemd ];
+    text = ''
+      device=${luksDevice}
+
+      if [ -z "''${device}" ]; then
+        echo "cryptenroll: no LUKS partition found in disko config" >&2;
+        exit 1
+      fi
+
+      exec systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=0,2,7 "''${device}" "$@"
+    '';
+  };
 in
 {
   options.hostSpec.boot.secureBoot.enable =
@@ -24,7 +74,11 @@ in
       initrd.systemd.enable = true;
     };
 
-    environment.persistence = mkIf impermanenceEnabled {
+    environment.systemPackages = mkIf (diskoLayout == "btrfs-luks" && cfg.secureBoot.enable) [
+      cryptenroll
+    ];
+
+    environment.persistence = mkIf (impermanenceEnabled && cfg.secureBoot.enable) {
       "/persist".directories = [
         "/var/lib/sbctl"
       ];
