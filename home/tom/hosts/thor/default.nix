8cb6638386205b2ff46b97ebfa787a80a8be193e
Tom Hesse <contact@tomhesse.xyz>
2025-12-07 12:37:48 +0100

    feat(home/tom/hosts/thor): add monitor config

--- home/tom/hosts/thor/default.nix
+++ home/tom/hosts/thor/default.nix
@@ -1,3 +1,15 @@
 {
-  homeSpec.browser.firefox.enable = true;
+  homeSpec = {
+    browser.firefox.enable = true;
+    monitors = [
+      {
+        name = "eDP-1";
+        primary = true;
+        width = 2880;
+        height = 1920;
+        refreshRate = 120;
+        scale = 2.0;
+      }
+    ];
+  };
 }
