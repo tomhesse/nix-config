a7721d95581c639f13c31808508231cd879faae3
Tom Hesse <contact@tomhesse.xyz>
2025-12-14 12:17:24 +0100

    chore(home/tom/hosts/tyr): add tyr monitor config

--- home/tom/hosts/tyr/default.nix
+++ home/tom/hosts/tyr/default.nix
@@ -4,15 +4,24 @@
     gaming.steam.autostart = true;
     monitors = [
       {
-        name = "DP-2";
+        name = "DP-1";
         primary = true;
         width = 2560;
         height = 1440;
         refreshRate = 144;
       }
       {
-        name = "";
-        enabled = false;
+        name = "HDMI-A-1";
+        width = 1920;
+        height = 1080;
+        position = "auto-left";
+        rotation = 1;
+      }
+      {
+        name = "DP-2";
+        width = 1920;
+        height = 1080;
+        position = "auto-right";
       }
     ];
   };
