ab225a92c416b7e4930a2d87d9665f1f14abeac0
Tom Hesse <contact@tomhesse.xyz>
2025-12-14 14:47:12 +0100

    chore(home/tom/desktop/hyprland): change vesktop workspace

--- home/tom/desktop/hyprland/windowrules.nix
+++ home/tom/desktop/hyprland/windowrules.nix
@@ -8,7 +8,7 @@ in
   wayland.windowManager.hyprland = mkIf enabled {
     settings = {
       windowrule = [
-        "workspace 3 silent, class:^(vesktop)$"
+        "workspace 5 silent, class:^(vesktop)$"
         "float, class:^(org\.pulseaudio\.pavucontrol)$"
         "opacity 0.95, focus:0"
         "opacity 1.00, focus:1"
