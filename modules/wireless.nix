295a3e7e08e3607abd635dea2216597fd14aa74d
Tom Hesse <contact@tomhesse.xyz>
2026-04-03 22:53:41 +0200

    feat(wireless): add wireless module with WPA3

---
+++ modules/wireless.nix
@@ -0,0 +1,21 @@
+{
+  flake.modules.nixos.wireless =
+    { config, ... }:
+    {
+      sops.secrets."wireless/Shrimphouse" = { };
+
+      sops.templates.wireless = {
+        content = ''
+          psk_Shrimphouse=${config.sops.placeholder."wireless/Shrimphouse"}
+        '';
+        restartUnits = [ "wpa_supplicant.service" ];
+      };
+
+      networking.wireless = {
+        enable = true;
+        fallbackToWPA2 = false;
+        secretsFile = config.sops.templates.wireless.path;
+        networks."Shrimphouse".pskRaw = "ext:psk_Shrimphouse";
+      };
+    };
+}
