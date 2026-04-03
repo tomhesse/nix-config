e39eb265f323cb66e4c515df7b2e7d0c4e5b5fe4
Tom Hesse <contact@tomhesse.xyz>
2026-04-03 15:04:21 +0200

    feat(sops): add shared sops module with age key discovery

---
+++ modules/sops/default.nix
@@ -0,0 +1,17 @@
+{ inputs, ... }:
+{
+  flake.modules.nixos.sops =
+    { config, ... }:
+    let
+      isEd25519 = key: key.type == "ed25519";
+      keys = builtins.filter isEd25519 config.services.openssh.hostKeys;
+    in
+    {
+      imports = [ inputs.sops-nix.nixosModules.sops ];
+
+      config.sops = {
+        defaultSopsFile = ./secrets.yaml;
+        age.sshKeyPaths = map (key: key.path) keys;
+      };
+    };
+}
