535292d2feb3daf9d419340b42e939b4f3671f03
Tom Hesse <contact@tomhesse.xyz>
2026-04-03 20:33:12 +0200

    feat(boot): add boot module with limine

---
+++ modules/boot.nix
@@ -0,0 +1,15 @@
+{
+  flake.modules.nixos.boot = {
+    boot = {
+      loader = {
+        limine = {
+          enable = true;
+          enableEditor = false;
+          maxGenerations = 10;
+        };
+        efi.canTouchEfiVariables = true;
+      };
+      initrd.systemd.enable = true;
+    };
+  };
+}
