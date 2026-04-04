ca410ce6614e5cd3db823534fdea59ebf149fbb4
Tom Hesse <contact@tomhesse.xyz>
2026-04-04 16:52:07 +0200

    feat(pipewire): add pipewire module with ALSA and PulseAudio support

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -2,6 +2,7 @@
 {
   flake.modules.nixos.desktop = {
     imports = [
+      self.modules.nixos.pipewire
       self.modules.nixos.wireless
     ];
   };
---
+++ modules/pipewire.nix
@@ -0,0 +1,11 @@
+{
+  flake.modules.nixos.pipewire = {
+    security.rtkit.enable = true;
+
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      pulse.enable = true;
+    };
+  };
+}
