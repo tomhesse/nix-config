a08a84e5671701311770f46085c351ee42cabf21
Tom Hesse <contact@tomhesse.xyz>
2026-04-05 10:36:19 +0200

    feat(xdg): add relative path helpers for impermanence

--- modules/xdg.nix
+++ modules/xdg.nix
@@ -1,5 +1,33 @@
 {
-  flake.modules.homeManager.xdg = {
-    xdg.enable = true;
-  };
+  flake.modules.homeManager.xdg =
+    { config, lib, ... }:
+    let
+      mkRelative = path: lib.removePrefix "${config.home.homeDirectory}/" path;
+      mkOpt =
+        description:
+        lib.mkOption {
+          type = lib.types.str;
+          readOnly = true;
+          inherit description;
+        };
+    in
+    {
+      options.xdg = {
+        relativeCacheHome = mkOpt "XDG cache directory relative to $HOME";
+        relativeConfigHome = mkOpt "XDG config directory relative to $HOME";
+        relativeDataHome = mkOpt "XDG data directory relative to $HOME";
+        relativeStateHome = mkOpt "XDG state directory relative to $HOME";
+      };
+
+      config = {
+        xdg = {
+          enable = true;
+
+          relativeCacheHome = mkRelative config.xdg.cacheHome;
+          relativeConfigHome = mkRelative config.xdg.configHome;
+          relativeDataHome = mkRelative config.xdg.dataHome;
+          relativeStateHome = mkRelative config.xdg.stateHome;
+        };
+      };
+    };
 }
