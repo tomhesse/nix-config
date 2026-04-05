8c9faeb2d732fc5a1a17b1e540325dac1f98d9cb
Tom Hesse <contact@tomhesse.xyz>
2026-04-05 15:23:37 +0200

    refactor(kitty): remove font package in favor of fonts module

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -1,14 +1,9 @@
 {
-  flake.modules.homeManager.kitty =
-    { pkgs, ... }:
-    {
-      programs.kitty = {
-        enable = true;
+  flake.modules.homeManager.kitty = {
+    programs.kitty = {
+      enable = true;
 
-        font = {
-          name = "FiraCode Nerd Font";
-          package = pkgs.nerd-fonts.fira-code;
-        };
-      };
+      font.name = "FiraCode Nerd Font";
     };
+  };
 }
