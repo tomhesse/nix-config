33093b3b03557731970ab911fc3c475bead90a39
Tom Hesse <contact@tomhesse.xyz>
2026-04-07 18:10:17 +0200

    feat(packages): add power-menu package

---
+++ packages/power-menu/package.nix
@@ -0,0 +1,38 @@
+{
+  coreutils,
+  hyprlock,
+  rofi,
+  systemd,
+  uwsm,
+  writeShellApplication,
+}:
+writeShellApplication {
+  name = "power-menu";
+  runtimeInputs = [
+    coreutils
+    hyprlock
+    rofi
+    systemd
+    uwsm
+  ];
+  text = ''
+    choice="$(
+      printf '%s\n' \
+        '  Shutdown' \
+        '  Reboot' \
+        '  Suspend' \
+        '  Logout' \
+        '  Lock' \
+      | rofi -dmenu -i -p 'Power Menu' | cut -d' ' -f2
+    )"
+
+    case "$choice" in
+      Shutdown) exec systemctl poweroff ;;
+      Reboot)   exec systemctl reboot ;;
+      Suspend)  exec systemctl suspend ;;
+      Lock)     exec hyprlock ;;
+      Logout)   exec uwsm stop ;;
+      *)        exit 0 ;;
+    esac
+  '';
+}
