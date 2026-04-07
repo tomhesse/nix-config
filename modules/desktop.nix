d854eedba3fe643b9daf01aaae0521b2367c02fd
Tom Hesse <contact@tomhesse.xyz>
2026-04-07 17:27:38 +0200

    feat(hyprlock): add wallpaper background from shared option

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -20,6 +20,7 @@
       self.modules.homeManager.base
       self.modules.homeManager.cliphist
       self.modules.homeManager.fonts
+      self.modules.homeManager.hyprlock
       self.modules.homeManager.kitty
       self.modules.homeManager.rofi
     ];
