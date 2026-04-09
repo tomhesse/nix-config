bcd9f0da9decb93b6656826a44b7a5ebb35fe588
Tom Hesse <contact@tomhesse.xyz>
2026-04-09 20:12:15 +0200

    feat(btop): add btop module with vim keys

---
+++ modules/btop.nix
@@ -0,0 +1,11 @@
+{
+  flake.modules.homeManager.btop = {
+    programs.btop = {
+      enable = true;
+      settings = {
+        vim_keys = true;
+        swap_disk = false;
+      };
+    };
+  };
+}
