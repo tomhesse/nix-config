265fd0b144383500d7208e3986ac39b2229e04e9
Tom Hesse <contact@tomhesse.xyz>
2026-04-11 14:54:40 +0200

    feat(tyr): add tyr desktop host

---
+++ modules/hosts/tyr/default.nix
@@ -0,0 +1,45 @@
+{ self, ... }:
+{
+  configurations.nixos.tyr.module = {
+    imports = [
+      self.diskoConfigurations.tyr
+      self.modules.nixos.desktop
+      self.modules.nixos.secure-boot
+      self.modules.nixos.user-thesse
+    ];
+
+    home-manager.users.thesse.imports = [ self.modules.homeManager.desktop ];
+
+    hardware.facter.reportPath = ./facter.json;
+
+    monitors = {
+      DP-1 = {
+        description = "ASUSTek COMPUTER INC VG27AQL1A R5LMQS197620";
+        resolution = "2560x1440";
+        refreshRate = 144;
+        position.x = 0;
+        position.y = 0;
+        primary = true;
+      };
+      DP-2 = {
+        description = "AOC 2490W1 APGL89A001738";
+        resolution = "1920x1080";
+        refreshRate = 60;
+        position.x = 2560;
+        position.y = 320;
+      };
+      HDMI-A-1 = {
+        description = "AOC 2490W1 APGL89A001447";
+        resolution = "1920x1080";
+        refreshRate = 60;
+        position.x = -1080;
+        position.y = -240;
+        rotation = "90";
+      };
+    };
+
+    networking.hostName = "tyr";
+
+    system.stateVersion = "25.11";
+  };
+}
---
+++ modules/hosts/tyr/disko.nix
@@ -0,0 +1,58 @@
+{
+  flake.diskoConfigurations.tyr = {
+    disko.devices.disk.main = {
+      type = "disk";
+      device = "/dev/disk/by-id/nvme-WD_BLACK_SN850X_2000GB_25074B4A1B02_1";
+      content = {
+        type = "gpt";
+        partitions = {
+          esp = {
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
+            };
+          };
+          luks = {
+            size = "100%";
+            content = {
+              type = "luks";
+              name = "tyr";
+              settings.allowDiscards = true;
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-Ltyr" ];
+                subvolumes = {
+                  "@" = {
+                    mountpoint = "/";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@persistent" = {
+                    mountpoint = "/persistent";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
