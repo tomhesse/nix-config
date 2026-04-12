64d15e5d1eb3bf9399c80d9ad26002b42d49014e
Tom Hesse <contact@tomhesse.xyz>
2026-04-12 11:08:36 +0200

    feat(fish): add tmux.fish plugin

--- modules/shells/fish.nix
+++ modules/shells/fish.nix
@@ -50,6 +50,15 @@
             (plugin "autopair")
             (plugin "git-abbr")
             (plugin "plugin-sudope")
+            {
+              name = "tmux";
+              src = pkgs.fetchFromGitHub {
+                owner = "budimanjojo";
+                repo = "tmux.fish";
+                rev = "db0030b7f4f78af4053dc5c032c7512406961ea5";
+                sha256 = "sha256-rRibn+FN8VNTSC1HmV05DXEa6+3uOHNx03tprkcjjs8=";
+              };
+            }
           ];
       };
     };
