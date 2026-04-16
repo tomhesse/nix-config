33ba70a037d1c357c21b942fe21a46624d612ae7
Tom Hesse <contact@tomhesse.xyz>
2026-04-16 12:37:51 +0200

    feat(loki): assign workspaces to monitors

--- modules/hosts/loki/default.nix
+++ modules/hosts/loki/default.nix
@@ -19,6 +19,13 @@
         refreshRate = 120;
         scale = 2;
         primary = true;
+        workspaces = [
+          1
+          2
+          3
+          4
+          5
+        ];
       };
     };
 
