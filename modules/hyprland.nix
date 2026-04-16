5ef7cbdfba3103b2af65db3dd764c21404ec3aa7
Tom Hesse <contact@tomhesse.xyz>
2026-04-16 17:36:49 +0200

    feat(hyprland): add workspace window rules for kitty, firefox, and zed

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -92,6 +92,10 @@
           windowrule = [
             "opacity 0.95, focus:0"
             "opacity 1.00, focus:1"
+
+            "workspace 1 silent, class:^(kitty)$"
+            "workspace 2 silent, class:^(firefox)$"
+            "workspace 3 silent, class:^(dev\.zed\.Zed)$"
           ];
           bind = [
             "$mainMod, V, exec, ${cliphist} list | ${uwsm} app -- ${rofi} -dmenu -display-columns 2 | ${cliphist} decode | ${wl-copy}"
