560074a674290c7c3f459e7bc76b77de6aa37c44
Tom Hesse <contact@tomhesse.xyz>
2026-04-16 18:15:39 +0200

    feat(imv): add imv image viewer module

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -28,6 +28,7 @@
       self.modules.homeManager.dunst
       self.modules.homeManager.firefox
       self.modules.homeManager.fonts
+      self.modules.homeManager.imv
       self.modules.homeManager.hyprland
       self.modules.homeManager.hypridle
       self.modules.homeManager.hyprlock
