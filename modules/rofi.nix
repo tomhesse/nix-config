39933c6ee7c4c92b88b852f6f5bbf95f998c6695
Tom Hesse <contact@tomhesse.xyz>
2026-04-18 22:40:15 +0200

    feat(rofi): persist entry history and druncache via impermanence

--- modules/rofi.nix
+++ modules/rofi.nix
@@ -1,13 +1,20 @@
 {
-  flake.modules.homeManager.rofi = {
-    programs.rofi = {
-      enable = true;
-      font = "Fira Sans 12";
-      extraConfig = {
-        run-command = "uwsm app -- {cmd}";
-        kb-row-up = "Up,Alt+k";
-        kb-row-down = "Down,Alt+j";
+  flake.modules.homeManager.rofi =
+    { config, ... }:
+    {
+      home.persistence."/persistent".files = [
+        "${config.xdg.relativeCacheHome}/rofi-entry-history.txt"
+        "${config.xdg.relativeCacheHome}/rofi3.druncache"
+      ];
+
+      programs.rofi = {
+        enable = true;
+        font = "Fira Sans 12";
+        extraConfig = {
+          run-command = "uwsm app -- {cmd}";
+          kb-row-up = "Up,Alt+k";
+          kb-row-down = "Down,Alt+j";
+        };
       };
     };
-  };
 }
