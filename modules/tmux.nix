6ca2f12637144ec5c472cec006a49762efea6415
Tom Hesse <contact@tomhesse.xyz>
2026-04-20 21:25:34 +0200

    feat(tmux): add continuum plugin for automatic session saving and restore

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -1,6 +1,9 @@
 {
   flake.modules.homeManager.tmux =
     { config, pkgs, ... }:
+    let
+      continuum = "${pkgs.tmuxPlugins.continuum}/share/tmux-plugins/continuum";
+    in
     {
       home.persistence."/persistent".directories = [
         "${config.xdg.relativeStateHome}/tmux/resurrect"
@@ -15,10 +18,12 @@
           bind | split-window -h -c "#{pane_current_path}"
           unbind '"'
           bind - split-window -v -c "#{pane_current_path}"
+
           set -g status-right-length 100
           set -g status-left-length 100
           set -g status-left ""
-          set -g status-right "#{E:@catppuccin_status_user}"
+          set -g status-right "#(${continuum}/scripts/continuum_save.sh)"
+          set -ag status-right "#{E:@catppuccin_status_user}"
           set -ag status-right "#{E:@catppuccin_status_host}"
           set -ag status-right "#{E:@catppuccin_status_date_time}"
         '';
@@ -32,6 +37,13 @@
             extraConfig = ''
               set -g @resurrect-dir '${config.xdg.stateHome}/tmux/resurrect'
               set -g @resurrect-capture-pane-contents 'on'
+            '';
+          }
+          {
+            plugin = pkgs.tmuxPlugins.continuum;
+            extraConfig = ''
+              set -g @continuum-restore 'on'
+              set -g @continuum-save-interval '10'
             '';
           }
         ];
