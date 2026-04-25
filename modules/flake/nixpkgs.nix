b9bf9d1ece04f29d87a43aaf59f6bfc24f0a522a
Tom Hesse <contact@tomhesse.xyz>
2026-04-25 18:28:37 +0200

    refactor(nixpkgs): merge flake input into module

--- modules/flake/nixpkgs.nix
+++
@@ -1,6 +0,0 @@
-{
-  flake-file.inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    nixpkgs-lib.follows = "nixpkgs";
-  };
-}
--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -1,4 +1,9 @@
 {
+  flake-file.inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs-lib.follows = "nixpkgs";
+  };
+
   flake.modules.nixos.nixpkgs =
     { lib, ... }:
     {
