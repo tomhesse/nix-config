fccec7fdc182c2b79099004280984c9bbb38f4c4
Tom Hesse <contact@tomhesse.xyz>
2026-04-29 18:46:44 +0200

    feat(tang): open firewall port from listenStream config

--- modules/tang.nix
+++ modules/tang.nix
@@ -1,12 +1,25 @@
 {
-  flake.modules.nixos.tang = {
-    services.tang = {
-      enable = true;
-      ipAddressAllow = [ "10.0.20.0/24" ];
-    };
+  flake.modules.nixos.tang =
+    { config, lib, ... }:
+    let
+      inherit (builtins) match elemAt;
+      extractPort =
+        s:
+        let
+          m = match ".*[:\\]]([0-9]+)" s;
+        in
+        if m != null then lib.toInt (elemAt m 0) else lib.toInt s;
+    in
+    {
+      services.tang = {
+        enable = true;
+        ipAddressAllow = [ "10.0.20.0/24" ];
+      };
 
-    environment.persistence."/persistent" = {
-      directories = [ "/var/lib/private/tang" ];
+      networking.firewall.allowedTCPPorts = map extractPort config.services.tang.listenStream;
+
+      environment.persistence."/persistent" = {
+        directories = [ "/var/lib/private/tang" ];
+      };
     };
-  };
 }
