2c3968ddd998730ef560f5f581945729f5003633
Tom Hesse <contact@tomhesse.xyz>
2026-05-01 10:57:40 +0200

    feat(mimir): add host configuration

---
+++ modules/hosts/mimir/default.nix
@@ -0,0 +1,26 @@
+{ self, ... }:
+{
+  configurations.nixos.mimir.module = {
+    imports = [
+      self.diskoConfigurations.mimir
+      self.modules.nixos.clevis
+      self.modules.nixos.common-cpu-intel
+      self.modules.nixos.common-pc-ssd
+      self.modules.nixos.secure-boot
+      self.modules.nixos.server
+      self.modules.nixos.user-thesse
+    ];
+
+    boot.initrd.availableKernelModules = [ "i40e" ];
+    boot.initrd.clevis.devices."mimir".secretFile = /persistent/secrets/clevis/mimir.jwe;
+
+    hardware.facter.reportPath = ./facter.json;
+
+    networking.hostId = "700e144e";
+    networking.hostName = "mimir";
+
+    system.stateVersion = "25.11";
+
+    time.timeZone = "Europe/Berlin";
+  };
+}
---
+++ modules/hosts/mimir/disko.nix
@@ -0,0 +1,260 @@
+{
+  flake.diskoConfigurations.mimir = {
+    disko.devices = {
+      disk = {
+        boot = {
+          type = "disk";
+          device = "/dev/disk/by-id/nvme-nvme.8086-50484d42383032323030384334383044474e-494e54454c2053534450454431443438304741-00000001";
+          content = {
+            type = "gpt";
+            partitions = {
+              esp = {
+                size = "512M";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = [ "umask=0077" ];
+                };
+              };
+              luks = {
+                size = "100%";
+                content = {
+                  type = "luks";
+                  name = "mimir";
+                  settings.allowDiscards = true;
+                  content = {
+                    type = "btrfs";
+                    extraArgs = [ "-Lmimir" ];
+                    subvolumes = {
+                      "@" = {
+                        mountpoint = "/";
+                        mountOptions = [
+                          "compress=zstd"
+                          "noatime"
+                        ];
+                      };
+                      "@nix" = {
+                        mountpoint = "/nix";
+                        mountOptions = [
+                          "compress=zstd"
+                          "noatime"
+                        ];
+                      };
+                      "@persistent" = {
+                        mountpoint = "/persistent";
+                        mountOptions = [
+                          "compress=zstd"
+                          "noatime"
+                        ];
+                      };
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+
+        rocket0 = {
+          type = "disk";
+          device = "/dev/disk/by-id/wwn-0x5002538f5531e4d4";
+          content = {
+            type = "gpt";
+            partitions.zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "rocket";
+              };
+            };
+          };
+        };
+        rocket1 = {
+          type = "disk";
+          device = "/dev/disk/by-id/wwn-0x5002538f5531e4d5";
+          content = {
+            type = "gpt";
+            partitions.zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "rocket";
+              };
+            };
+          };
+        };
+
+        tank0 = {
+          type = "disk";
+          device = "/dev/disk/by-id/wwn-0x5000cca278d64ea0";
+          content = {
+            type = "gpt";
+            partitions.zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "tank";
+              };
+            };
+          };
+        };
+        tank1 = {
+          type = "disk";
+          device = "/dev/disk/by-id/wwn-0x5000cca278d64f1f";
+          content = {
+            type = "gpt";
+            partitions.zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "tank";
+              };
+            };
+          };
+        };
+        tank2 = {
+          type = "disk";
+          device = "/dev/disk/by-id/wwn-0x5000cca278d63046";
+          content = {
+            type = "gpt";
+            partitions.zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "tank";
+              };
+            };
+          };
+        };
+        tank3 = {
+          type = "disk";
+          device = "/dev/disk/by-id/wwn-0x5000cca27ac59544";
+          content = {
+            type = "gpt";
+            partitions.zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "tank";
+              };
+            };
+          };
+        };
+        tank4 = {
+          type = "disk";
+          device = "/dev/disk/by-id/wwn-0x5000cca278d63e40";
+          content = {
+            type = "gpt";
+            partitions.zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "tank";
+              };
+            };
+          };
+        };
+        tank5 = {
+          type = "disk";
+          device = "/dev/disk/by-id/wwn-0x5000cca278d67452";
+          content = {
+            type = "gpt";
+            partitions.zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "tank";
+              };
+            };
+          };
+        };
+        tank6 = {
+          type = "disk";
+          device = "/dev/disk/by-id/wwn-0x5000cca278d67451";
+          content = {
+            type = "gpt";
+            partitions.zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "tank";
+              };
+            };
+          };
+        };
+        tank7 = {
+          type = "disk";
+          device = "/dev/disk/by-id/wwn-0x5000cca278d5e643";
+          content = {
+            type = "gpt";
+            partitions.zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "tank";
+              };
+            };
+          };
+        };
+      };
+
+      zpool = {
+        rocket = {
+          type = "zpool";
+          mode = "mirror";
+          options.ashift = "12";
+          rootFsOptions = {
+            atime = "off";
+            canmount = "off";
+            compression = "zstd";
+            encryption = "aes-256-gcm";
+            keyformat = "raw";
+            keylocation = "file:///persistent/secrets/zfs/rocket.key";
+            mountpoint = "none";
+          };
+        };
+
+        tank = {
+          type = "zpool";
+          mode = {
+            topology = {
+              type = "topology";
+              vdev = [
+                {
+                  mode = "raidz2";
+                  members = [
+                    "tank0"
+                    "tank2"
+                    "tank4"
+                    "tank6"
+                  ];
+                }
+                {
+                  mode = "raidz2";
+                  members = [
+                    "tank1"
+                    "tank3"
+                    "tank5"
+                    "tank7"
+                  ];
+                }
+              ];
+            };
+          };
+          options.ashift = "12";
+          rootFsOptions = {
+            atime = "off";
+            canmount = "off";
+            compression = "zstd";
+            encryption = "aes-256-gcm";
+            keyformat = "raw";
+            keylocation = "file:///persistent/secrets/zfs/tank.key";
+            mountpoint = "none";
+          };
+        };
+      };
+    };
+  };
+}
