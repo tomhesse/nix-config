79121aece54b410a236b7153330967f0cde610a0
Tom Hesse <contact@tomhesse.xyz>
2026-05-01 10:21:29 +0200

    feat(clevis): add initrd DHCP network config for Tang

--- modules/clevis.nix
+++ modules/clevis.nix
@@ -1,10 +1,18 @@
 {
   flake.modules.nixos.clevis = {
-    boot.initrd.clevis = {
-      enable = true;
-      useTang = true;
-    };
+    boot.initrd = {
+      clevis = {
+        enable = true;
+        useTang = true;
+      };
 
-    boot.initrd.systemd.network.enable = true;
+      systemd.network = {
+        enable = true;
+        networks."10-dhcp" = {
+          matchConfig.Type = "ether";
+          networkConfig.DHCP = "yes";
+        };
+      };
+    };
   };
 }
