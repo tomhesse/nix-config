19db9de20d7b1d971d0a212b21d73d4d77ddb5a2
Tom Hesse <contact@tomhesse.xyz>
2026-05-04 21:43:46 +0200

    feat(navidrome): disable downloads

--- modules/navidrome.nix
+++ modules/navidrome.nix
@@ -10,6 +10,7 @@
             Port = 4533;
             MusicFolder = "/srv/media/music";
             EnableUserEditing = false;
+            EnableDownloads = false;
             ExtAuth.TrustedSources = "127.0.0.1/32";
             ExtAuth.LogoutURL = "https://idm.shrimphouse.xyz/ui/logout";
             Plugins.Enabled = false;
