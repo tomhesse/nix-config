ef628e17fee99bf1cc5700151f1592538bc9d7a6
Tom Hesse <contact@tomhesse.xyz>
2026-05-16 01:15:48 +0200

    fix(sabnzbd): grant write access to radarr and sonarr on complete directory

--- modules/sabnzbd.nix
+++ modules/sabnzbd.nix
@@ -4,8 +4,8 @@
       "d /var/tmp/sabnzbd 0755 sabnzbd sabnzbd -"
       "d /var/tmp/sabnzbd/complete 0755 sabnzbd sabnzbd -"
       "d /var/tmp/sabnzbd/incomplete 0750 sabnzbd sabnzbd -"
-      "a+ /var/tmp/sabnzbd/complete - - - - default:user:radarr:rX,user:radarr:rX"
-      "a+ /var/tmp/sabnzbd/complete - - - - default:user:sonarr:rX,user:sonarr:rX"
+      "a+ /var/tmp/sabnzbd/complete - - - - default:user:radarr:rwX,user:radarr:rwX"
+      "a+ /var/tmp/sabnzbd/complete - - - - default:user:sonarr:rwX,user:sonarr:rwX"
     ];
 
     services = {
