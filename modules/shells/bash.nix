98d0df15a3fb0a0b8cc68bae1c335203c8bdeddc
Tom Hesse <contact@tomhesse.xyz>
2026-06-09 08:04:43 +0200

    feat(bash): add tmux aliases and autostart with session management

--- modules/shells/bash.nix
+++ modules/shells/bash.nix
@@ -18,6 +18,60 @@
           "ignoredups"
           "ignorespace"
         ];
+        initExtra = ''
+          TMUX_DEFAULT_SESSION_NAME="Main"
+          TMUX_AUTOSTART=true
+          TMUX_AUTOSTART_ONCE=true
+          TMUX_AUTOCONNECT=true
+          TMUX_AUTOQUIT=false
+
+          function tmux {
+            if [[ $# -eq 0 ]]; then
+              if [[ "$TMUX_AUTOCONNECT" == true ]]; then
+                command tmux new-session -A -s "$TMUX_DEFAULT_SESSION_NAME"
+              else
+                command tmux new-session -s "$TMUX_DEFAULT_SESSION_NAME"
+              fi
+            else
+              command tmux "$@"
+            fi
+          }
+
+          function tds {
+            local dir=''${PWD##*/}
+            local md5
+            if command -v md5sum &>/dev/null; then
+              md5=$(printf '%s' "$PWD" | md5sum | cut -d ' ' -f 1)
+            elif command -v md5 &>/dev/null; then
+              md5=$(printf '%s' "$PWD" | md5)
+            else
+              printf 'tds: md5sum or md5 not found\n' >&2
+              return 1
+            fi
+            command tmux new-session -As "''${dir}-''${md5:0:6}"
+          }
+
+          if [[ "$TMUX_AUTOSTART" == true ]] && [[ -z "$TMUX" ]] && [[ -z "$SSH_CONNECTION" ]] && [[ -t 1 ]]; then
+            if [[ "$TMUX_AUTOSTART_ONCE" != true ]] || [[ -z "$TMUX_AUTOSTARTED" ]]; then
+              export TMUX_AUTOSTARTED=true
+              if [[ "$TMUX_AUTOCONNECT" == true ]]; then
+                command tmux new-session -A -s "$TMUX_DEFAULT_SESSION_NAME"
+              else
+                command tmux new-session -s "$TMUX_DEFAULT_SESSION_NAME"
+              fi
+              [[ "$TMUX_AUTOQUIT" == true ]] && exit
+            fi
+          fi
+        '';
+        shellAliases = {
+          ta = "tmux attach -t";
+          tad = "tmux attach -d -t";
+          tkss = "tmux kill-session -t";
+          tksv = "tmux kill-server";
+          tl = "tmux list-sessions";
+          to = "tmux new-session -A -s";
+          ts = "tmux new-session -s";
+        };
       };
     };
 }
