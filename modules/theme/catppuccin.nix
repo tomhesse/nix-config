bddcdbba03c701bb8b226e05ec6a6b966c2ee1e3
Tom Hesse <contact@tomhesse.xyz>
2026-06-21 19:11:22 +0200

    fix(catppuccin): disable gemini-cli theming until catppuccin/nix supports 26.11

--- modules/theme/catppuccin.nix
+++ modules/theme/catppuccin.nix
@@ -18,6 +18,7 @@
     catppuccin = {
       enable = true;
       cursors.enable = true;
+      gemini-cli.enable = false; # TODO: remove after update to 26.11 (https://github.com/catppuccin/nix/issues/974)
       tmux.extraConfig = ''
         set -g @catppuccin_window_status_style "rounded"
         set -g @catppuccin_date_time_text " %H:%M"
