3924db18406504d18a728fad526a34ce19ecabaf
Tom Hesse <contact@tomhesse.xyz>
2026-09-05 23:20:32 +0200

    refactor(server): drop nginx and acme in favour of traefik

--- modules/server.nix
+++ modules/server.nix
@@ -2,11 +2,9 @@
 {
   flake.modules.nixos.server = {
     imports = [
-      self.modules.nixos.acme
       self.modules.nixos.base
       self.modules.nixos.lts-kernel
       self.modules.nixos.msmtp
-      self.modules.nixos.nginx
     ];
   };
 
