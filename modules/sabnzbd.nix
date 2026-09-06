12cc9e3e8714933a2c3aadf2771b5052e687a425
Tom Hesse <contact@tomhesse.xyz>
2026-09-06 18:21:09 +0200

    feat(sabnzbd): add the download client

--- modules/sabnzbd.nix
+++ modules/sabnzbd.nix
@@ -1,127 +1,47 @@
 {
   flake.modules.nixos.sabnzbd =
-    { config, ... }:
-    {
-      systemd.tmpfiles.rules = [
-        "d /var/cache/sabnzbd 0755 sabnzbd sabnzbd -"
-        "d /var/cache/sabnzbd/complete 0755 sabnzbd sabnzbd -"
-        "d /var/cache/sabnzbd/incomplete 0750 sabnzbd sabnzbd -"
-        "a+ /var/cache/sabnzbd/complete - - - - default:user:radarr:rwX,user:radarr:rwX"
-        "a+ /var/cache/sabnzbd/complete - - - - default:user:sonarr:rwX,user:sonarr:rwX"
+    let
+      domain = "shrimphouse.xyz";
+
+      stages = [
+        "/srv/downloads/complete"
+        "/srv/downloads/incomplete"
       ];
+    in
+    {
+      virtualisation.oci-containers.containers.sabnzbd = {
+        image = "lscr.io/linuxserver/sabnzbd:5.1.2-ls271";
 
-      services = {
-        sabnzbd = {
-          enable = true;
-          secretFiles = [ config.sops.templates."sabnzbd-secrets".path ];
-          settings = {
-            categories = {
-              "*" = {
-                name = "*";
-                order = 0;
-                pp = 3;
-                script = "None";
-              };
-              audio = {
-                name = "audio";
-                order = 3;
-                pp = "";
-                priority = -100;
-                script = "Default";
-              };
-              movies = {
-                name = "movies";
-                order = 1;
-                pp = "";
-                priority = -100;
-                script = "Default";
-              };
-              software = {
-                name = "software";
-                order = 4;
-                pp = "";
-                priority = -100;
-                script = "Default";
-              };
-              tv = {
-                name = "tv";
-                order = 2;
-                pp = "";
-                priority = -100;
-                script = "Default";
-              };
-            };
-            misc = {
-              bandwidth_max = "125M";
-              bandwidth_perc = 80;
-              cache_limit = "24G";
-              complete_dir = "/var/cache/sabnzbd/complete";
-              complete_free = "10G";
-              direct_unpack = true;
-              download_dir = "/var/cache/sabnzbd/incomplete";
-              download_free = "100G";
-              email_account = "608deeb4-b226-44f7-bb38-4354d8029c7e";
-              email_endjob = "never";
-              email_from = "sabnzbd@mail.shrimphouse.xyz";
-              email_server = "smtp.tem.scw.cloud";
-              email_to = "arr@shrimphouse.xyz,";
-              enable_recursive = true;
-              fulldisk_autoresume = true;
-              history_limit = 10;
-              host_whitelist = "sabnzbd.shrimphouse.xyz,";
-              permissions = 775;
-            };
-            servers."news.eweka.nl" = {
-              connections = 50;
-              displayname = "news.eweka.nl";
-              expire_date = "2027-08-16";
-              host = "news.eweka.nl";
-              name = "news.eweka.nl";
-              username = "4110a8a7db7608de";
-            };
-          };
+        networks = [ "edge" ];
+
+        volumes = [
+          "/srv/services/sabnzbd:/config"
+        ]
+        ++ map (path: "${path}:${path}") stages;
+
+        environment = {
+          PUID = "404";
+          PGID = "404";
+          TZ = "Europe/Berlin";
         };
 
-        oauth2-proxy.nginx.virtualHosts."sabnzbd.shrimphouse.xyz".allowed_groups = [
-          "arr_users@shrimphouse.xyz"
-        ];
-
-        nginx.virtualHosts."sabnzbd.shrimphouse.xyz" = {
-          useACMEHost = "sabnzbd.shrimphouse.xyz";
-          forceSSL = true;
-
-          locations."/".proxyPass = "http://127.0.0.1:8080";
+        labels = {
+          "traefik.enable" = "true";
+          "traefik.http.routers.sabnzbd.rule" = "Host(`sabnzbd.${domain}`)";
+          "traefik.http.routers.sabnzbd.middlewares" = "authelia@docker";
         };
+
+        extraOptions = [ "--security-opt=no-new-privileges" ];
       };
 
-      sops = {
-        secrets =
-          let
-            sopsFile = ./hosts/${config.networking.hostName}/secrets/nixos.yaml;
-          in
-          {
-            "services/sabnzbd/api-key" = { inherit sopsFile; };
-            "services/sabnzbd/nzb-key" = { inherit sopsFile; };
-            "services/sabnzbd/email-pwd" = { inherit sopsFile; };
-            "services/sabnzbd/eweka-password" = { inherit sopsFile; };
-          };
+      systemd = {
+        services.podman-sabnzbd = {
+          after = [ "zfs-mount.service" ];
 
-        templates."sabnzbd-secrets" = {
-          owner = "sabnzbd";
-          restartUnits = [ "sabnzbd.service" ];
-          content = ''
-            [misc]
-            api_key = ${config.sops.placeholder."services/sabnzbd/api-key"}
-            nzb_key = ${config.sops.placeholder."services/sabnzbd/nzb-key"}
-            email_pwd = ${config.sops.placeholder."services/sabnzbd/email-pwd"}
-
-            [servers]
-            [[news.eweka.nl]]
-            password = ${config.sops.placeholder."services/sabnzbd/eweka-password"}
-          '';
+          unitConfig.AssertPathIsMountPoint = stages ++ [ "/srv/services/sabnzbd" ];
         };
-      };
 
-      security.acme.certs."sabnzbd.shrimphouse.xyz".group = "nginx";
+        tmpfiles.rules = [ "d /srv/services/sabnzbd 0700 sabnzbd sabnzbd -" ];
+      };
     };
 }
