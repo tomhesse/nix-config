d9bceec647ff81c86ec8b086f0b4243bc99b8cd1
Tom Hesse <contact@tomhesse.xyz>
2026-09-07 17:41:45 +0200

    refactor(arr): generate the sonarr and radarr modules from a helper

--- modules/arr.nix
+++ modules/arr.nix
@@ -1,18 +1,83 @@
-{ self, ... }:
+{ lib, ... }:
 {
-  flake.modules.nixos.arr = {
-    imports = [
-      self.modules.nixos.bazarr
-      self.modules.nixos.exportarr-bazarr
-      self.modules.nixos.exportarr-prowlarr
-      self.modules.nixos.exportarr-radarr
-      self.modules.nixos.exportarr-sonarr
-      self.modules.nixos.prowlarr
-      self.modules.nixos.radarr
-      self.modules.nixos.recyclarr
-      self.modules.nixos.sabnzbd
-      self.modules.nixos.sabnzbd-exporter
-      self.modules.nixos.sonarr
-    ];
-  };
+  flake.modules.nixos =
+    let
+      domain = "shrimphouse.xyz";
+
+      images = {
+        radarr = "lscr.io/linuxserver/radarr:6.3.0.10514-ls314";
+        sonarr = "lscr.io/linuxserver/sonarr:4.0.19.2979-ls323";
+      };
+
+      mkArr =
+        {
+          app,
+          image,
+          libraries,
+          uid,
+        }:
+        let
+          mounts = libraries ++ [ "/srv/downloads/complete" ];
+        in
+        {
+          virtualisation.oci-containers.containers.${app} = {
+            inherit image;
+
+            networks = [ "edge" ];
+
+            volumes = [
+              "/srv/services/${app}:/config"
+            ]
+            ++ map (path: "${path}:${path}") mounts;
+
+            environment = {
+              PUID = toString uid;
+              PGID = toString uid;
+              UMASK = "002";
+              TZ = "Europe/Berlin";
+              "${lib.toUpper app}__AUTH__METHOD" = "Forms";
+              "${lib.toUpper app}__AUTH__REQUIRED" = "DisabledForLocalAddresses";
+            };
+
+            labels = {
+              "traefik.enable" = "true";
+              "traefik.http.routers.${app}.rule" = "Host(`${app}.${domain}`)";
+              "traefik.http.routers.${app}.middlewares" = "authelia@docker";
+            };
+
+            extraOptions = [ "--security-opt=no-new-privileges" ];
+          };
+
+          systemd = {
+            services."podman-${app}" = {
+              after = [ "zfs-mount.service" ];
+
+              unitConfig.AssertPathIsMountPoint = mounts ++ [ "/srv/services/${app}" ];
+            };
+
+            tmpfiles.rules = [ "d /srv/services/${app} 0700 ${app} ${app} -" ];
+          };
+        };
+    in
+    {
+      radarr = mkArr {
+        app = "radarr";
+        image = images.radarr;
+        libraries = [
+          "/srv/media/video/anime/movies"
+          "/srv/media/video/movies"
+        ];
+        uid = 402;
+      };
+
+      sonarr = mkArr {
+        app = "sonarr";
+        image = images.sonarr;
+        libraries = [
+          "/srv/media/video/anime/shows"
+          "/srv/media/video/shows"
+        ];
+        uid = 401;
+      };
+    };
 }
