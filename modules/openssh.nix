19d31c8bc2c2ecda1cb689a535431b7f0d600567
Tom Hesse <contact@tomhesse.xyz>
2026-09-26 19:41:27 +0200

    fix(openssh): trust the storage box sub-accounts

--- modules/openssh.nix
+++ modules/openssh.nix
@@ -67,7 +67,11 @@ in
           "github.com".publicKeyFile = githubHostKeys;
           "[u591202.your-storagebox.de]:23" = {
             publicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA5EB5p/5Hp3hGW1oHok+PIOH9Pbn7cnUiGmUEBrCVjnAw+HrKyN8bYVV0dIGllswYXwkG/+bgiBlE6IVIBAq+JwVWu1Sss3KarHY3OvFJUXZoZyRRg/Gc/+LRCE7lyKpwWQ70dbelGRyyJFH36eNv6ySXoUYtGkwlU5IVaHPApOxe4LHPZa/qhSRbPo2hwoh0orCtgejRebNtW5nlx00DNFgsvn8Svz2cIYLxsPVzKgUxs8Zxsxgn+Q/UvR7uq4AbAhyBMLxv7DjJ1pc7PJocuTno2Rw9uMZi1gkjbnmiOh6TTXIEWbnroyIhwc8555uto9melEUmWNQ+C+PwAK+MPw==";
-            extraHostNames = [ "[u591202-sub1.your-storagebox.de]:23" ];
+            extraHostNames = [
+              "[u591202-sub1.your-storagebox.de]:23"
+              "[u591202-sub2.your-storagebox.de]:23"
+              "[u591202-sub3.your-storagebox.de]:23"
+            ];
           };
         };
     };
